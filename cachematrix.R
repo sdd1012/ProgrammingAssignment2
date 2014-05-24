315e641a0aa34b93baccdd4a21079905d0ede069
Sudip Das <sudipdas@hotmail.com>
2014-05-24 00:06:17 -0400

    R file changed locally

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains 2 functions, makeCacheMatrix and cacheSolve, that
+## work in conjuction to compute the inverse of an invertible matrix
+## and store the computed state for recurring need
 
-## Write a short comment describing this function
 
+## The makeCacheMatrix creates a special "matrix" object, which is a list, 
+## from matrix 'x' as argument and can cache the inverse of 'x'
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                # reset inverse as special matrix has changed
+                inv <<- NULL 
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+        
 }
 
 
-## Write a short comment describing this function
-
+## The cacheSolve returns the inverse of the special matrix 'x' 
+## from its cache or after computing when not in cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(is.matrix(inv)) {
+                #debug message below, used for testing
+                #message("getting cached data")
+                
+                return(inv)
+        }
+        data <- x$get()
+        # Create an identity matrix b of same dimensions and
+        # pass it to 'solve' function to compute ONLY inverse
+        b <- diag(nrow(data))
+        inv <- solve(data, b, ...)
+        
+        x$setinverse(inv)
+        inv
 }
